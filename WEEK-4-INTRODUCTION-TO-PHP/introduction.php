d9fc29aeb7e4973e3be43a7fb351eb9271d4f950
Peter Muhumuki <muhumukip@gmail.com>
2024-10-14 13:54:02 +0300

    Added assignments 1, 2 and 3

---
+++ WEEK-4-INTRODUCTION-TO-PHP/introduction.php
@@ -0,0 +1,8 @@
+<?php
+$course = "Computer Science";
+$rollNumber = "None";
+$technology = "Artificial Intelligence.";
+
+echo "COURSE: $course <br>";
+echo "ROLL NUMBER: $rollNumber<br>";
+echo "TECHNOLOGY: $technology<br>";
---
+++ WEEK-5-BASIC-STRUCTURE-OF-PHP/Basic Structure.php
@@ -0,0 +1,11 @@
+<?php 
+$name = "Peter Muhumuki";
+$course = "Computer Science";
+$year = 3;
+$specialization = "Artificial Intelligence.";
+
+echo "NAME: $name<br>";
+echo "COURSE: $course<br>";
+echo "YEAR: $year<br>";
+echo "SPECIALIZATION: $specialization<br>";
+
