4712a9305fd06877fb00c6dd1d8b2a1e65d1261d
Peter Muhumuki <muhumukip@gmail.com>
2024-10-15 09:26:50 +0300

    Updating the assignments.

---
+++ PHP-CHAPTER-3-DATA-TYPES/data-types.php
@@ -0,0 +1,16 @@
+<?php
+
+// part 1.
+echo "Employee code is 001 <br>";
+echo "Employee name is ABC<br>";
+echo "Employee salary is 25000<br>";
+echo "Working hours are 7.5<br>";
+
+
+// part 2. Using variables.
+$employee_code = 001;
+$employee_name = "ABC";
+$employee_salary = 25000;
+$working_hours = 7.5;
+
+echo "Employee number ".$employee_code." is ".$employee_name.".<br> The receive a salary of ".$employee_salary." and works for ".$working_hours." hours daily.";
